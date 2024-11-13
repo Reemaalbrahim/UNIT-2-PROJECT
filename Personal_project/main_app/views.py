56d5640dc88796f196a29dba714cd6227c8e5e21
Reema <reemaalbrahim28@gmail.com>
2024-11-13 10:02:49 +0300

    Done from the messages and resume pages

--- Personal_project/main_app/views.py
+++ Personal_project/main_app/views.py
@@ -28,8 +28,24 @@ def contact_view(request):
     return render(request, 'main_app/contact.html')
 
 def messages_view(request):
-    messages = Message.objects.all()  
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        message_text = request.POST.get('message')
+
+        message = Message.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            message=message_text
+        )
+        message.save()
+        return redirect('main_app:messages_view')  # Redirect to the messages view after saving
+
+    messages = Message.objects.all()
     return render(request, 'main_app/messages.html', {'messages': messages})
+
 
 def resume_view(request):
     return render(request, 'main_app/resume.html')

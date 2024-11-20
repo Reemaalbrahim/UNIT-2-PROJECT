1652533d8ef96c89a44b08f9aad3d0c5e40b04ba
Reema <reemaalbrahim28@gmail.com>
2024-11-20 14:29:17 +0300

    Adding email verfication

--- Personal_project/Personal_project/settings.py
+++ Personal_project/Personal_project/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,6 +154,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
+
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT='587'
+EMAIL_USE_TLS= True
+EMAIL_HOST_USER=os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=os.environ.get("EMAIL_HOST_PASSWORD")
 
 STATICFILES_DIRS = [
     # 'C:\\Users\\rayoo\\Documents\\UNIT-2-PROJECT\\Personal_project\\static',  # Uncomment if needed
--- Personal_project/main_app/views.py
+++ Personal_project/main_app/views.py
@@ -1,33 +1,57 @@
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from .models import Message  
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.shortcuts import render
+from django.conf import settings
+
+
 
 def home_view(request):
     return render(request, 'main_app/home.html')
 
-from django.shortcuts import render
-from django.core.mail import send_mail
-from .models import Message  
+
 
 def contact_view(request):
-    success_message = None  # Initialize the success message variable
-    if request.method == 'POST':
+
+   if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
         message = request.POST['message']
 
-        
         if not Message.objects.filter(first_name=first_name, last_name=last_name, email=email,message=message).exists():
             new_message = Message(first_name=first_name, last_name=last_name, email=email,message=message)
             new_message.save()
-            success_message = "Message sent successfully!"  # Set the success message
+
+            messages.success(request, "Your message has been sent successfully!")
+
+            #send confirmation email 
+            content_html= render_to_string("main_app/mail/confirmation.html")
+            send_to=email
+            email_message= EmailMessage("Confirmation Message",content_html, settings.EMAIL_HOST_USER, [send_to])
+            email_message.content_subtype ="html"
+            email_message.send()
+
+            content_html= f"user {first_name} {last_name} contacted you." + f"<br/> {message}"
+            send_to=email
+            email_message= EmailMessage("Confirmation",content_html, settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER])
+            email_message.content_subtype ="html"
+            email_message.send()
+            #
 
 
         else:
-            success_message.info(request, "It seems you've already submitted this message.")
-        
-    return render(request, 'main_app/contact.html')
+            messages.info(request, "It seems you've already submitted this message.")
+
+
+        return redirect("main_app:contact_view")
+   
+   return render(request, 'main_app/contact.html')
+
+
 
 
 def messages_view(request):
